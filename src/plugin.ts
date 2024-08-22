98e1245e6c1987fb71dcd793eedfc2478e50dd20
gentlementlegen <fernand.veyrier@epitech.eu>
2024-08-22 13:03:14 +0900

    chore: fix type plural

--- src/plugin.ts
+++ src/plugin.ts
@@ -20,6 +20,6 @@ export async function plugin(inputs: PluginInputs, env: Env) {
     logger: new Logs("debug"),
   };
 
-  context.logger.info("Will check the following repo", { ...context.config.repos });
+  context.logger.info("Will check the following repos", { ...context.config.repos });
   return await updatePullRequests(context);
 }
